92286dc38387bc8d6efade293dbc2c07b28d1a54
7bs2 <2718360317@qq.com>
2021-12-15 14:41:35 +0800

    change

--- src/RemoveElements.java
+++ src/RemoveElements.java
@@ -1,7 +1,22 @@
 public class RemoveElements {
+
+    // 给你一个数组 和一个值 val，你需要原地移除所有数值等于val 的元素并返回移除后数组的新长度。
     public static void main(String[] args) {
-
+        int[] arr = new int[] {1,2,3,2,5,6,7};
+        System.out.println(removeElement(arr, 2));
     }
-
-    
+    public static int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        // left 与 right 相等的情况也需要检测一次
+        while (left <= right) {
+            // 将right的值给left直到left不等于val
+            if (nums[left] == val) {
+                nums[left] = nums[right--];
+            }else {
+                left++;
+            }
+        }
+        return left;
+    }
 }
