09aea5803fdc639200259e7484ba48e8d9991e17
7bs2 <2718360317@qq.com>
2021-12-31 15:01:52 +0800

    fib

---
+++ src/LeetCode/FibonacciOfSum.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+public class FibonacciOfSum {
+    public static void main(String[] args) {
+        System.out.println(fib2(25));
+        System.out.println(fib1(25));
+    }
+
+    // 求数列的第n项   此方法存在多次计算重复值的问题  时间复杂度过高
+    public static long fib1(long num) {
+       if (num == 1 || num == 2) return 1;
+       return fib1(num - 1) + fib1(num - 2);
+    }
+
+
+    public static int fib2(int num) {
+        int[] arr = new int[num + 1];
+        arr[1] = 1;
+        arr[2] = 1;
+        return fun(arr, num);
+    }
+
+    private static int fun(int[] arr, int num) {
+        if (num == 1 || num == 2) return 1;
+        if(arr[num] != 0) {
+            return arr[num];
+        }else {
+            arr[num] = fun(arr, num - 1) + fun(arr, num - 2);
+        }
+        return arr[num];
+    }
+
+}
--- src/LeetCode/ThreeNumbersFactor.java
+++ src/LeetCode/ThreeNumbersFactor.java
@@ -12,9 +12,44 @@ public class ThreeNumbersFactor {
     // 若数组有正负    最后三个正数的乘积 或   前两个负数与最后一个正数的乘积
     // 综上所述    最大三位数的乘积 一定为后三位  或者   前两位加最后一位
 
-    public static int maxFactor(int[] arr) {
+    public static int maxFactor1(int[] arr) {
         Arrays.sort(arr);
         return Math.max(arr[0] * arr[arr.length - 1] * arr[arr.length - 2],
                 arr[arr.length - 1] * arr[arr.length - 2] * arr[arr.length - 3]);
+    }
+
+
+
+    // 线性扫描  不使用排序
+    public static int maxFactor2(int[] arr) {
+
+        // min1为最小值   min2 为第二小
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        int max1 = Integer.MIN_VALUE, max2 =  Integer.MIN_VALUE, max3 =  Integer.MIN_VALUE;
+
+        for (int item : arr) {
+            // 如果比最小的还要小
+            if (item < min1) {
+                min2 = min1;
+                min1 = item;
+            }else if(item < min2) {
+                min2 = item;
+            }
+
+            // max1为最大   比最大的都要大
+            if (item > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = item;
+            }else if(item > max2) {
+                max3 = max2;
+                max2 = item;
+            }else if (item > max3) {
+                max3 = item;
+            }
+        }
+
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
     }
 }
