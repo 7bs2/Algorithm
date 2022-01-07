0331bb4bf2e48e2d48ead720c9cee56a1a37dd30
7bs2 <2718360317@qq.com>
2022-01-07 14:40:00 +0800

    bubble

---
+++ src/Sort/BubbleSort.java
@@ -0,0 +1,22 @@
+package Sort;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+}
